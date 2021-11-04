309cab90b8e174cad4070d25a4d5ec04198ffe3d
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-11-04 18:38:25 -0300

    feat: Add github oauth authentication method

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth';
+// eslint-disable-next-line import/no-unresolved
+import GithubProvider from 'next-auth/providers/github';
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: 'read:user',
+    }),
+  ],
+});
