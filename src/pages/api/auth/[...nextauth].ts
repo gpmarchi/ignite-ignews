b44860befb11dc98591594be8f41e6cc67cb6e86
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-11-05 18:35:47 -0300

    feat: Save user to the database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,31 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
-// eslint-disable-next-line import/no-unresolved
-import GithubProvider from 'next-auth/providers/github';
+import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
-    GithubProvider({
+    Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user',
     }),
   ],
+  jwt: {
+    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
